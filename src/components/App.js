0dae69826d2a526270c181442253254503281b07
zempo <zelenkosolomon@gmail.com>
2021-01-04 22:18:34 -0800

    add fonts and main theme colors

--- src/components/App.js
+++ src/components/App.js
@@ -8,9 +8,10 @@ import NavBar from "./static/NavBar";
 import Footer from "./static/Footer";
 
 // routes
-import Login from "../components/auth/Login";
+// import Login from "../components/auth/Login";
 import Register from "../components/auth/Register";
 // import ResetPassword from "../components/auth/ResetPassword"
+import Home from "./routes/Home";
 
 function App() {
   return (
@@ -19,12 +20,11 @@ function App() {
         <NavBar />
       </header>
       <main>
-        <h1>Plants</h1>
         <Switch>
+          <Route path='/' component={Home} />
           <Route path='/register' component={Register} />
-          <Route path='/login' component={Login} />
+          {/* <Route path='/login' component={Login} /> */}
           {/* <Route path='/resetpassword' component={ResetPassword}/> */}
-
           <Link to='/register'>Register</Link>
         </Switch>
       </main>
---
+++ src/components/routes/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section>
+      <h1>Water My Plants</h1>
+      <h2>Test header</h2>
+      <p>Test paragraph</p>
+    </section>
+  );
+};
+
+export default Home;
